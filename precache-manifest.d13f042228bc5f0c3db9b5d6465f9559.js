f8088851c8a5bb0b55ef3ac62ea66a212261b0b7
Débora Serra <debora.r.serra@gmail.com>
2022-04-18 17:34:34 -0300

    Updates

---
+++ precache-manifest.d13f042228bc5f0c3db9b5d6465f9559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50dfd45ab1f8edb70c46ef92ca16406",
+    "url": "/project-starwars-planets/index.html"
+  },
+  {
+    "revision": "94926e4dfdca66df6756",
+    "url": "/project-starwars-planets/static/css/main.5c323ebd.chunk.css"
+  },
+  {
+    "revision": "5f7815cd3802ba526da1",
+    "url": "/project-starwars-planets/static/js/2.ecf4fc33.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-starwars-planets/static/js/2.ecf4fc33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94926e4dfdca66df6756",
+    "url": "/project-starwars-planets/static/js/main.91b2fc43.chunk.js"
+  },
+  {
+    "revision": "6803ff4fcf5fdc941d6f",
+    "url": "/project-starwars-planets/static/js/runtime-main.2fb71acd.js"
+  },
+  {
+    "revision": "5d1001864d6c0efe802f0964d8b54325",
+    "url": "/project-starwars-planets/static/media/Star_Wars_logo-1.5d100186.png"
+  }
+]);
